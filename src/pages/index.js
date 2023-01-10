995b08d8b459d928bb8f88570619d1b54512e3ac
Lukas Alois Zbornik <lukasalois.zbornik@plus4u.net>
2023-01-10 22:41:29 +0100

    Project init and installation of necessary packages

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Lukáš Alois Zborník</title>
+      </Head>
+      <h1 style={{width: "100%", textAlign: "center", marginTop: "45vh"}}>Lukáš Alois Zborník</h1>
+    </>
+  )
+}
